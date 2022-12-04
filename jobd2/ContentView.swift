da3637e03c9f1ee9eb85ffcd875fd3e370ef7cdb
JoelHaa <joel.haapaniemi@outlook.com>
2022-12-04 15:08:21 +0200

    Changed the default text and image

--- jobd2/ContentView.swift
+++ jobd2/ContentView.swift
@@ -10,11 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "car")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+            Text("This application will show OBD2 diagnostics and fault codes. Ideally will be compatible with JDM cars.")        }
         .padding()
     }
 }
