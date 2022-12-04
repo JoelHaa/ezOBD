370bfdd3ef86539b652ee9179377cafaf8887a63
Joel Haapaniemi <joel.haapaniemi@outlook.com>
2022-12-04 14:04:37 +0200

    Initial Commit

---
+++ jobd2/jobd2App.swift
@@ -0,0 +1,17 @@
+//
+//  jobd2App.swift
+//  jobd2
+//
+//  Created by Joel Haapaniemi on 4.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct jobd2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
